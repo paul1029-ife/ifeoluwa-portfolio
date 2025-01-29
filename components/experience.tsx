5e8c94e4d8bb9dd0e5cb04fad2055c6212b55e5e
TheOne001 <paul.agbogun@gmail.com>
2025-01-29 02:47:11 +0100

    Change images for the projects section.

--- components/experience.tsx
+++ components/experience.tsx
@@ -77,7 +77,7 @@ const education: ExperienceData[] = [
       "Data Structures",
       "Algorithms",
       "Software Engineering",
-      "Web Development",
+      "Low-level programming",
     ],
     highlights: [
       "Maintaining strong academic performance",
