7f1452f3348293a3b3bc40426883d9cc04a0bfad
TheOne001 <paul.agbogun@gmail.com>
2025-03-21 07:00:20 +0100

    Update current projects.

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 // Define valid project IDs
 const validProjects = {
   tbc: "tbc",
-  nibo: "nibo",
+  sparkedge: "sparkedge",
   "be-you": "beyou",
 } as const;
 
